d0d254c7acd8dfe30ba62300fb4ef10c020206f7
Matej Nemcek <ybdaba@gmail.com>
2017-04-01 16:39:32 +0200

    standard enabled

--- zilch.js
+++ zilch.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
 const blake = require('blakejs')
-const clipboardy = require('clipboardy');
+const clipboardy = require('clipboardy')
 const inquirer = require('inquirer')
 const meow = require('meow')
 const niceware = require('niceware')
@@ -10,27 +10,28 @@ const cli = meow(`
   Usage
   $ zilch domain <even-sized entropy> <joinChar> <clipboard-timeout-in-ms>`)
 const i = { domain: cli.input[0],
-          entropy: cli.input[1],
-          joinChar: cli.input[2],
-          clipboardtimeout: cli.input[3],
-          backupClipboard: clipboardy.readSync()
-        }
+  entropy: cli.input[1],
+  joinChar: cli.input[2],
+  clipboardtimeout: cli.input[3],
+  backupClipboard: clipboardy.readSync()
+}
 
 // default config
-if(i.entropy === undefined)
+if (i.entropy === undefined) {
   i.entropy = 6
-
-if(i.joinChar === undefined)
+}
+if (i.joinChar === undefined) {
   i.joinChar = '+'
+}
 
-if(i.clipboardtimeout === undefined)
+if (i.clipboardtimeout === undefined) {
   i.clipboardtimeout = 5000
+}
 
-inquirer.prompt([
-  {
-    type: 'password',
-    message: 'Enter your masterpass',
-    name: 'mpw'
+inquirer.prompt([{
+  type: 'password',
+  message: 'Enter your masterpass',
+  name: 'mpw'
 }
 ]).then(function (a) {
   const mpw = blake.blake2bHex(a.mpw, a.mpw)
@@ -41,9 +42,9 @@ inquirer.prompt([
   const arr = niceware.bytesToPassphrase(buffer)
   const password = arr.join(i.joinChar)
   clipboardy.writeSync(password)
-  setTimeout(function() {
+  setTimeout(function () {
     clipboardy.writeSync(i.backupClipboard)
-  }, i.clipboardtimeout);
-}).catch(function(err) {
+  }, i.clipboardtimeout)
+}).catch(function (err) {
   console.error(err.message)
 })
